519886f40726c838ae7eeb2b39c1fd7c023ee4b5
Andrew Jones <me@andrewhjon.es>
2013-10-16 20:12:32 -0400

    Represent the operations as macros
    The function evaluation has to be lazy otherwise it doesn't work.

--- src/csp/core.clj
+++ src/csp/core.clj
@@ -12,29 +12,38 @@
     stop
     'bleep))
 
-(defn prefix
+(defmacro prefix
   "the operation (c->P) 'c then P'"
   [c P]
-  (fn [x]
-    (if (= x c)
-      P
-      'bleep)))
+  `(fn [x#]
+     (if (= x# ~c)
+       ~P
+       '~'bleep)))
 
-(defn choice2
+(defmacro choice2
   "The binary choice (c->P|d->Q) 'c then P choose d then Q'"
   [c P d Q]
-  (fn [x]
-    (if (= x c)
-      P
-      (if (= x d)
-        Q
-        'bleep))))
+  `(fn [x#]
+     (if (= x# ~c)
+       ~P
+       (if (= x# ~d)
+         ~Q
+         '~'bleep))))
 
 (def vending
   "mu X . coin -> choc -> X"
-  (prefix 'coin (prefix 'choc #'vending)))
+  (prefix 'coin (prefix 'choc vending)))
+
+(defn ct
+  [n]
+  (if (= n 0)
+    (choice2 'around (ct 0) 'up (ct 1))
+    (choice2 'up (ct (+ n 1)) 'down (ct (- n 1)))))
 
 (defn example
   [x]
   (assert (= 'beep
-             ( ( ( ( (vending 'coin) 'choc) 'coin) 'choc) 'other))))
+             ( ( ( ( (vending 'coin) 'choc) 'coin) 'choc) 'other)))
+
+  (assert (= 'bleep
+             ( ( ( (ct 0) 'around) 'up) 'around))))
